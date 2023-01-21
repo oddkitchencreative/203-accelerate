1cf6ee58c063f6e89a74bb5935067b0651703067
oddkitchencreative <sarah@oddkitchencreative.com>
2023-01-20 22:55:26 -0600

    Updated case study CSS again

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -9,12 +9,12 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
+		</div><!-- #content -->
 
 	</div><!-- #primary -->
 
